6c517a6cb33d77c6d3c313f956756febf922d66f
Ishan <Ojhaishan007@gmail.com>
2023-04-21 09:36:01 +0545

    password added and hashed in db

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var con =  require('./connection');
 var express = require('express');
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -17,13 +20,18 @@ var lastname = req.body.lastName;
 var NID = req.body.nationalId;
 var emaill = req.body.email;
 var PN = req.body.phoneNumber;
-con.connect(function(error){
-if (error) throw error;
-var sql = "INSERT INTO users(first_name,middle_name,last_name,national_id,email,phone_number)VALUES('"+firstname+"','"+middlename+"','"+lastname+"','"+NID+"','"+emaill+"','"+PN+"')";
-con.query(sql,function(error,result){
-    if (error) throw error;
-    res.send('RECORD SUCCESSFULL'+ console.log("success"));
-});
-});
+var PW = req.body.password;
+ // Hash the password using bcrypt
+ bcrypt.hash(PW, saltRounds, function(err, hash) {
+    if (err) throw err;
+    con.connect(function(error) {
+      if (error) throw error;
+      var sql = "INSERT INTO users(first_name,middle_name,last_name,national_id,email,phone_number,password) VALUES ('" + firstname + "','" + middlename + "','" + lastname + "','" + NID + "','" + emaill + "','" + PN + "','" + hash + "')";
+      con.query(sql, function(error, result) {
+        if (error) throw error;
+        res.send('RECORD SUCCESSFULL' + console.log("success"));
+      });
+    });
+  });
 });
 app.listen(5501);
